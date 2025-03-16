16ddc165ff32999ea889b2b3f10f2dcb25b50830
magnus <magnus@udel.edu>
2025-03-16 18:23:13 -0400

    added GiveAttempts component

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer,
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+    const correct: string = answer === expectedAnswer ? "✔️" : "❌";
     return (
         <div>
-            <h3>Check Answer</h3>
+            <Form.Group controlId="formShortAnswerResponse">
+                <Form.Label>Answer: </Form.Label>
+                <Form.Control
+                    value={answer}
+                    onChange={(e) => {
+                        setAnswer(e.target.value);
+                    }}
+                />
+            </Form.Group>
+            <div>{correct}</div>
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,32 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): React.JSX.Element {
+    const [attempts, setAttempts] = useState<number>(3);
+    const [request, setRequest] = useState<number>(0);
+    function addRequest(): void {
+        setAttempts(attempts + request);
+    }
+    function use(): void {
+        setAttempts(attempts - 1);
+    }
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <div>attempts left: {attempts}</div>
+            <Form.Group controlId="formGiveAttempts">
+                <Form.Label>Request Attempts</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={request}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                        setRequest(parseInt(event.target.value) || 0);
+                    }}
+                />
+            </Form.Group>
+            <Button onClick={addRequest}>gain</Button>
+            <Button onClick={use} disabled={attempts <= 0}>
+                use
+            </Button>
         </div>
     );
 }
