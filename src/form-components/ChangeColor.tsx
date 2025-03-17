d1765132912c22f94addcf94ee512db32ddbae99
magnus <magnus@udel.edu>
2025-03-16 21:28:50 -0400

    added ChangeColor component, INLINE DID NOT WORK

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,48 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+const COLORS = [
+    "red",
+    "blue",
+    "green",
+    "orange",
+    "purple",
+    "cyan",
+    "magenta",
+    "white",
+    "black",
+];
 
 export function ChangeColor(): React.JSX.Element {
+    const [color, setColor] = useState<string>(COLORS[0]);
+    function updateColor(event: React.ChangeEvent<HTMLInputElement>) {
+        setColor(event.target.value);
+    }
     return (
         <div>
-            <h3>Change Color</h3>
+            <Form.Group controlId="forColorSelect">
+                <Form.Label>Change Color</Form.Label>
+                {COLORS.map((choice: string) => (
+                    <div key={choice}>
+                        <Form.Check
+                            inline
+                            type="radio"
+                            label={choice}
+                            value={choice}
+                            checked={color === choice}
+                            onChange={updateColor}
+                            name="color"
+                        />
+                    </div>
+                ))}
+            </Form.Group>
+            <div
+                data-testid="colored-box"
+                style={{
+                    background: color,
+                }}
+            >
+                You have chosen {color}
+            </div>
         </div>
     );
 }
