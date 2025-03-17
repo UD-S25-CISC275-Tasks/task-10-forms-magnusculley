ca2425695479a399c3946819f33a05c5002f7b17
magnus <magnus@udel.edu>
2025-03-16 20:59:45 -0400

    added EditMode component

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,56 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
+    const [isEditMode, setIsEditMode] = useState<boolean>(false);
+    const [name, setName] = useState<string>("Your Name");
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                id="edit-mode-switch"
+                label="Edit Mode"
+                className="form-switch"
+                checked={isEditMode}
+                onChange={() => {
+                    setIsEditMode(!isEditMode);
+                }}
+            />
+
+            {isEditMode ?
+                <div>
+                    <Form.Group controlId="formName">
+                        <Form.Label>Name:</Form.Label>
+                        <Form.Control
+                            type="text"
+                            value={name}
+                            onChange={(
+                                event: React.ChangeEvent<HTMLInputElement>,
+                            ) => {
+                                setName(event.target.value);
+                            }}
+                        />
+                    </Form.Group>
+
+                    <Form.Group controlId="formStudent">
+                        <Form.Check
+                            type="checkbox"
+                            label="Student"
+                            checked={isStudent}
+                            onChange={(
+                                event: React.ChangeEvent<HTMLInputElement>,
+                            ) => {
+                                setIsStudent(event.target.checked);
+                            }}
+                        />
+                    </Form.Group>
+                </div>
+            :   <p>
+                    {name} {isStudent ? "is a student" : "is not a student"}
+                </p>
+            }
         </div>
     );
 }
